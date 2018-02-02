bea8c0e19619063e439984322b51180f29ad8997
akayne <15270231+arielkayne@users.noreply.github.com>
2018-02-02 15:05:57 -0600

    frontpage/home changes. still need css work

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,8 +2,15 @@ import React from "react";
 
 const Home = () => {
 	return (
-		<div>Home</div>
+		<div>Home 
+		<br/>
+		<img src="http://get-a-wingman.com/wp-content/uploads/2015/10/How-to-Groom-a-Beard-660x273.jpg"/>
+		<form action="./Login"><input type="submit" value="Login"/></form> 
+		<form action="./Register"><input type="submit" value="Register"/></form> 
+		<form action="./Gallery"><input type="submit" value="Member Gallery"/></form> 
+		</div>
 		)
 };
+
 
 export default Home
