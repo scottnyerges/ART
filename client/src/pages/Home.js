3fc30a5f13d3c5301bf21ac359d37dc7e2fcbe68
Scott Nyerges <32683265+scottnyerges@users.noreply.github.com>
2018-02-04 15:29:04 -0600

    Update Home.js
    adds buttons to login/register page, plus ancillary display text

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const Home = () => {
 	return (
-		<div>Home</div>
+		<div>
+		<h1>AustinArtFNDR v1.0</h1> 
+		<h2>Welcome.</h2>
+		<h3>Already a member? Log in!</h3>
+		<form action="/login"><input type="submit" value="Login"/></form> 
+		<h3>Create an account!</h3>
+		<form action="/register"><input type="submit" value="Register"/></form> 
+		</div>
 		)
 };
+
 
 export default Home
