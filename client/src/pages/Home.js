6d122810eaaca85fb7ef4f674c69a1c6b183534f
Scott Nyerges <32683265+scottnyerges@users.noreply.github.com>
2018-02-04 16:29:12 -0600

    Update Home.js
    adds graphic

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,12 +3,20 @@ import React from "react";
 const Home = () => {
 	return (
 		<div>
-		<h1>AustinArtFNDR v1.0</h1> 
-		<h2>Welcome.</h2>
-		<h3>Already a member? Log in!</h3>
-		<form action="/login"><input type="submit" value="Login"/></form> 
-		<h3>Create an account!</h3>
-		<form action="/register"><input type="submit" value="Register"/></form> 
+			<div class="row">
+				<h1>AustinArtFNDR v1.0</h1> 
+				<h2>Welcome.</h2>
+			</div>
+			<div class="row">	
+			<img src={require("../logo.png")} />
+				<div class="column">
+					<h3>Already a member? Log in!</h3>
+						<form action="/login"><input type="submit" value="Login"/></form> 
+						<h3>Create an account!</h3>
+						<form action="/register"><input type="submit" value="Register"/></form>
+				</div>	
+					 
+			</div>
 		</div>
 		)
 };
