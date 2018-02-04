1805df2551c03cf6f28ff14254a0474959c30c8b
Scott Nyerges <32683265+scottnyerges@users.noreply.github.com>
2018-02-03 20:47:27 -0600

    Update Gallery.js
    throws error unexpected token at json line 20

--- client/src/pages/Gallery.js
+++ client/src/pages/Gallery.js
@@ -1,19 +1,51 @@
 import React from "react";
 import axios from "axios";
 
-// class Gallery extends React.Component {
-// 	state = {
-// 		username:"",
-// 		password:"",
-// 		website:"",
-// 		image:"",
-// 		medium:"",
-// 	};
+class Gallery extends React.Component {
 
-const Gallery = () => {
-	return (
-		<div>Gallery</div>
-		)
-};
+    constructor() {
+        super();
+
+        // this sets the initial state, an empty array of members
+        this.state = {
+            members: [],
+        };
+    }
+
+    componentWillMount() {
+        // CALL THE API, our list of users; trying to call from the model account.js, which should be where userdata is....
+        fetch(
+            "./Account"
+        ).then(
+            results => results.json()
+            ).then(
+            data => {
+
+                // this maps over the data
+                let members = data.results.map((member, index) => {
+                    return (
+                        <div key={member.results}>
+                            {/*SELECT WHAT DATA TO RETURN*/}
+                            <li>{member.username}</li>
+                        </div>
+                    )
+                });
+
+                // here, we reset the member array state from empty to "full"
+                console.log("state", this.state.members);
+                this.setState({ members: members });
+            }
+            );
+    }
+
+    render() {
+        return (
+            <div>
+                {/*render the data list here; what goes to the gallery page*/}
+                {this.state.members}
+            </div>
+        )
+    }
+}
 
 export default Gallery
